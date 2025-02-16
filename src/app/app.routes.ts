19dbdd3f17852052a079898781318499d7712f48
SamSantosSouza <santossamuell1235@gmail.com>
2025-02-16 13:46:14 -0300

    feat: adicionando as rotas para fazer a nevegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import path from 'path';
 import { MainComponent } from './main/main.component';
+import { OriginsComponent } from './origins/origins.component';
+import { NgModule } from '@angular/core';
+import { CampeonatosComponent } from './campeonatos/campeonatos.component';
+import { RulesComponent } from './rules/rules.component';
 
 export const routes: Routes = [
     {path: "", component: MainComponent},
+    {path: "origins", component: OriginsComponent},
+    {path: "camp", component: CampeonatosComponent},
+    {path: "rules", component: RulesComponent}
+
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
